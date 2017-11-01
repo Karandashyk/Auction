698b9ae6b2bf388e8f98a26c30fd1c3a355198a4
Karandashyk <d.sharshakova@gmail.com>
2017-11-01 21:18:36 +0200

    Users setup.(not complete)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  get 'welcome/index'
+  get  '/signup',  to: 'users#new'
+  root 'welcome#index'
 end
