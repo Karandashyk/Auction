c14ec64710b488798667fd9453bddfcfc2bfe04a
Karandashyk <d.sharshakova@gmail.com>
2017-12-13 13:24:41 +0200

    Design implemintations

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   get 'persons/profile'
 
   get '/about_us', to: 'welcome#about'
   root 'welcome#index'
 
+  resources :users, only: [:show]
   resources :users_products
   resources :deals
   resources :money
